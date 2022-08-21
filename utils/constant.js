245229c1f4f637405183a1a8e779289391790ea4
Pharanyu <pharanyuchuenjit@gmail.com>
2022-08-21 22:03:13 +0100

    debug constant file

--- utils/constant.js
+++ utils/constant.js
@@ -1,12 +1,10 @@
 const NOT_FOUND_ERROR_CODE = 404;
-const NOT_FOUND_ERROR_MSG = { message: `${Object.values(err.errors)}` };
 const ERROR_OCCURED_CODE = 500;
 const ERROR_OCCURED_MSG = 'An error occured';
 const INVALID_CODE = 400;
 const SUCCESS_CODE = 200;
 module.exports = {
   NOT_FOUND_ERROR_CODE,
-  NOT_FOUND_ERROR_MSG,
   ERROR_OCCURED_CODE,
   ERROR_OCCURED_MSG,
   INVALID_CODE,
